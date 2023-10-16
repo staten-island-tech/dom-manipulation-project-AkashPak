5713d1f2bfe9f4deb9dea9755253415651ecf510
AkashPak <akashp8@nycstudents.net>
2023-10-16 09:26:21 -0400

    dawd

---
+++ dom.js
@@ -0,0 +1,15 @@
+const form= document.querySelector("#form")
+console.log(form)
+
+const DomSelectors= {
+    form: document.querySelector("form"),
+    FirstName: document.querySelector(".first-name"),
+    h2s: document.querySelectorAll("h2") 
+}
+
+DomSelectors.form.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log(DomSelectors.FirstName.value);
+    DomSelectors.h2s.forEach(
+        (el)=> el.textContent = DomSelectors.FirstName.value)
+});
