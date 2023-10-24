3d7bcf52221b1af7b52d1595d41035f7bf591c01
AkashPak <akashp8@nycstudents.net>
2023-10-24 09:26:15 -0400

    dadaw

--- dom.js
+++ dom.js
@@ -3,7 +3,7 @@ console.log(form)
 
 const DomSelectors= {
     form: document.querySelector("form"),
-    FirstName: document.querySelector(".first-name"),
+    card: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2") 
 }
 
