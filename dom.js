2a83a44186fcf42e4c93fb2aa49b0c31fdcb3581
AkashPak <akashp8@nycstudents.net>
2023-10-26 09:26:09 -0400

    daw

--- dom.js
+++ dom.js
@@ -3,13 +3,27 @@ console.log(form)
 
 const DomSelectors= {
     form: document.querySelector("form"),
-    card: document.querySelector(".first-name"),
+    AlbumName: document.querySelector(".album-name"),
     h2s: document.querySelectorAll("h2") 
+}
+const DomSelectors1={
+    form: document.querySelector("form"),
+    AlbumCover: document.querySelector(".album-cover"),
+    h3s: document.querySelector("h3")
 }
 
 DomSelectors.form.addEventListener("submit",function(event){
     event.preventDefault();
-    console.log(DomSelectors.FirstName.value);
+    console.log(DomSelectors.AlbumName.value);
     DomSelectors.h2s.forEach(
-        (el)=> el.textContent = DomSelectors.FirstName.value)
+        (el)=> el.textContent = DomSelectors.AlbumName.value)
 });
+
+DomSelectors1.form.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log(DomSelectors1.AlbumCover.value);
+
+  
+});
+
+
