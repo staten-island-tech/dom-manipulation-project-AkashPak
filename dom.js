581bffbfc29df46162b52e2b40153328eea4f7a1
AkashPak <144455859+AkashPak@users.noreply.github.com>
2023-10-26 08:45:57 -0400

    adw
    dawd

--- dom.js
+++ dom.js
@@ -3,7 +3,7 @@ console.log(form)
 
 const DomSelectors= {
     form: document.querySelector("form"),
-    card: document.querySelector(".first-name"),
+    FirstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2") 
 }
 
